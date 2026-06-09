1dde8801963f0ccca499200a0f40e0cd02010d81
ChanTheMan96 <monkeycity230@gmail.com>
2026-06-09 00:46:52 -0500

    fix key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  clerkPublishableKey: '',
+  production: true,
+  clerkPublishableKey: 'pk_test_cmlnaHQtcHJhd24tMC5jbGVyay5hY2NvdW50cy5kZXYk',
   apiBible: {
     baseUrl: '/api/bible',
     apiKey: '',
-    defaultBibleId: '78a9f6124f344018-01'
-  }
+    defaultBibleId: '78a9f6124f344018-01',
+  },
 };
