89e2a4655c25d7db8b5baf4c428ca01dc584aa33
ChanTheMan96 <monkeycity230@gmail.com>
2026-06-20 14:46:15 -0500

    fix pay

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -6,11 +6,11 @@ interface DonationAmount {
 }
 
 @Component({
-    selector: 'app-donate',
-    templateUrl: './donate.component.html',
-    styleUrls: ['./donate.component.scss'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+  selector: 'app-donate',
+  templateUrl: './donate.component.html',
+  styleUrls: ['./donate.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class DonateComponent {
   private readonly paypalEmail = 'betterinchristsupport@gmail.com';
@@ -18,7 +18,7 @@ export class DonateComponent {
     { label: '$5', value: 5 },
     { label: '$15', value: 15 },
     { label: '$25', value: 25 },
-    { label: '$50', value: 50 }
+    { label: '$50', value: 50 },
   ];
   selectedAmount = 15;
 
@@ -28,7 +28,8 @@ export class DonateComponent {
       business: this.paypalEmail,
       item_name: 'Better in Christ',
       currency_code: 'USD',
-      amount: String(this.selectedAmount)
+      amount: String(this.selectedAmount),
+      no_shipping: '1',
     });
 
     return `https://www.paypal.com/cgi-bin/webscr?${params.toString()}`;
